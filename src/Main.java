55a5c503df143b1ce3d96e7ded7ef3a5e0469b85
yali <yusufali0147@hotmail.com>
2023-09-05 10:06:12 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+ public class Main {
+     public static void main(String[] args) {
+         System.out.println("Üç Basamaklı Amstrong Sayıları Bulma");
+
+         int number,result, pow, basvalue, basnumber, temp;
+
+         for (number = 99; number <= 999; number++) {
+             temp = number;
+             result = 0;
+             while (temp != 0) {
+                 basvalue = temp % 10;
+                 pow = 1;
+                 basnumber = 3;
+                 for (int i = 1; i <= basnumber; i++) {
+                     pow *= basvalue;
+                 }
+                 result += pow;
+                 temp /= 10;
+             }
+             if (result == number) {
+                 System.out.println(number + " sayısı bir Armstrong sayıdır.");
+             }
+         }
+         System.out.println("Geri Kalan Üç Basamaklı Sayılar Amstrong Sayı Değildir. ");
+     }
+ }
